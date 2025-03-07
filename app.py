148002e2a1cd968258f5130d30ea3b29c7eef9d1
Ermias <123589824+ErmiasKetos@users.noreply.github.com>
2025-03-07 05:16:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -392,7 +392,7 @@ def create_pdf_report(lab_name, lab_address, lab_email, lab_phone, cover_data, p
 
     # Insert the KELP logo at the top-left
     try:
-        pdf.image("kelp_logo.png", x=10, y=10, w=80)
+        pdf.image("kelp_logo.png", x=10, y=1, w=80)
     except Exception as e:
         pdf.set_font("DejaVu", "", 10)
         pdf.cell(0, 10, "[Logo not found]", ln=True, align="L")
